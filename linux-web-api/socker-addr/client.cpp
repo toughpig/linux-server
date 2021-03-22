2d08c0bdd07625b6a0fec4e1c39a7210fa38620e
toughpig <wh19@mail.ustc.edu.cn>
2021-03-22 16:58:10 +0800

    first

---
+++ linux-web-api/socker-addr/client.cpp
@@ -0,0 +1,62 @@
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<bits/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<cassert>
+#include<csignal>
+#include<unistd.h>
+
+static char IP[]{"127.0.0.1"};
+static int PORT{8888};
+static int BACKLOG{5};
+
+void connect_and_send()
+{
+	int port = PORT;
+	int backlog = BACKLOG;
+//ADDR
+	struct sockaddr_in addr;
+	bzero(&addr, sizeof(addr));
+	addr.sin_family = AF_INET;
+	inet_aton(IP,&addr.sin_addr);
+	addr.sin_port = htons(port);
+//socket
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	assert(sock>=0);
+
+    if(connect(sock, (struct sockaddr*)&addr,sizeof(addr))<0){
+        printf("connection failed\n");
+    }
+    
+    char msg[255];
+    while(1){
+        scanf("%s",msg);
+        send(sock,msg,strlen(msg),0);
+    }
+    
+    close(sock);
+	
+}
+
+
+int main(int argc, char* argv[])
+{
+	if(argc>1)
+	{
+		if(argc!=3) printf("Usage: ./client ip port\n");
+		strncpy(IP,argv[1],strlen(argv[1]));
+		PORT = atoi(argv[2]);
+	}
+
+	connect_and_send();
+
+	printf("sock closed\n");
+
+	return 0;
+}
+
+
---
+++ linux-web-api/socker-addr/server.cpp
@@ -0,0 +1,77 @@
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<bits/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<cassert>
+#include<csignal>
+#include<unistd.h>
+
+static char IP[]{"127.0.0.1"};
+static int PORT{8888};
+static int BACKLOG{5};
+
+int bind_and_listen()
+{
+	int port = PORT;
+	int backlog = BACKLOG;
+//ADDR
+	struct sockaddr_in addr;
+	bzero(&addr, sizeof(addr));
+	addr.sin_family = AF_INET;
+	inet_aton(IP,&addr.sin_addr);
+	addr.sin_port = htons(port);
+//socket
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	assert(sock>=0);
+//bind
+	int ret = bind(sock, (struct sockaddr*)&addr, sizeof(addr));
+	assert(ret!=-1);
+//listen
+	ret = listen(sock, backlog);
+	assert(ret!=-1);
+	
+	return sock;
+}
+
+void accept_and_receive(int sock)
+{
+	struct sockaddr_in client;
+	socklen_t client_len = sizeof(client);
+	int connfd = accept(sock,(struct sockaddr*)&client,&client_len);
+	assert(connfd>=0);
+	printf("accept\n");
+	printf("client addr: %s:%d\n",inet_ntoa(client.sin_addr),client.sin_port);
+	char msg[255];
+
+	int ret;
+	while( (ret = recv(connfd,msg,254,0)) > 0 ){
+		printf("get %d bytes of message %s\n",ret,msg);
+		memset(msg,0,255);
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	if(argc>1)
+	{
+		if(argc!=3) printf("Usage: ./server ip port\n");
+		strncpy(IP,argv[1],strlen(argv[1]));
+		PORT = atoi(argv[2]);
+	}
+
+	int sock = bind_and_listen();
+
+	accept_and_receive(sock);
+
+	close(sock);
+
+	printf("sock closed\n");
+
+	return 0;
+}
+
+
